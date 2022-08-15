d4fe419fddc0227476258608bfb61526360a5a79
Rafael Triantafillidis <botazz133@gmail.com>
2022-08-15 19:40:48 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {IndexComponent} from './components/index/index.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		IndexComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		ServiceWorkerModule.register('ngsw-worker.js', {
+			enabled: environment.production,
+			// Register the ServiceWorker as soon as the app is stable
+			// or after 30 seconds (whichever comes first).
+			registrationStrategy: 'registerWhenStable:30000'
+		})
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule {
+}
