c5eaebab3aa3891a51c3483bc0a3f64f6fcea891
Rafael Triantafillidis <botazz133@gmail.com>
2022-10-04 20:46:53 +0300

    Remove back button functionality from main component

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,17 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MatButtonToggle, MatButtonToggleChange, MatButtonToggleGroup} from "@angular/material/button-toggle";
+import {fromEvent, Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
 	selector: 'app-index',
 	templateUrl: './index.component.html',
 	styleUrls: ['./index.component.scss']
 })
-export class IndexComponent implements OnInit {
-	//TODO truncate player names
+export class IndexComponent implements OnInit, OnDestroy {
+	private readonly browserHistorySubject: Subject<void>;
+
+	// TODO truncate player names
 	constructor() {
+		this.browserHistorySubject = new Subject<void>();
 	}
 
 	ngOnInit(): void {
+		this.removeBackButtonFunctionality();
+	}
+
+	private removeBackButtonFunctionality(): void {
+		history.pushState(null, '');
+		fromEvent(window, 'popstate').pipe(
+			takeUntil(this.browserHistorySubject)
+		).subscribe((_) => {
+			history.pushState(null, '');
+		});
+	}
+
+	ngOnDestroy(): void {
+		this.browserHistorySubject.next();
+		this.browserHistorySubject.complete();
 	}
 
 	toggleChange(event: MatButtonToggleChange) {
