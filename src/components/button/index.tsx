1ac40aed48587a26a9a315c77ac511dc522ae63e
Rafael Triantafillidis <botazz133@gmail.com>
2023-03-05 21:08:35 +0200

    Scaffold next app

---
+++ src/components/button/index.tsx
@@ -0,0 +1,15 @@
+import {type NextPage} from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const MyButton: NextPage = () => {
+    return (
+        <>
+            <button className='bg-orange-600'>
+                hello
+            </button>
+        </>
+    );
+};
+
+export default MyButton;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import {type NextPage} from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <div className='underline text-5xl text-center'>
+                hi
+            </div>
+        </>
+    );
+};
+
+export default Home;
