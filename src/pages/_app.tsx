8ad4bdd4932cd8a2282578f1452bf824287b260f
Rafael Triantafillidis <botazz133@gmail.com>
2023-03-05 21:11:14 +0200

    Install mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import {type AppType} from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const MyApp: AppType = ({Component, pageProps}) => {
   return <Component {...pageProps} />;
