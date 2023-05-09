ffac7edcf8c1e100b58d2201baef1c460a1c8834
Rafael Triantafillidis <botazz133@gmail.com>
2023-05-09 21:30:20 +0300

    Display game ended dialog

--- src/components/set-score-button/index.tsx
+++ src/components/set-score-button/index.tsx
@@ -1,15 +1,49 @@
 import { Typography } from '@mui/material';
-import { Link } from '@tanstack/router';
+import { useNavigate } from '@tanstack/router';
+import { useSelector } from 'react-redux';
+import { CURRENT_TURN_DETAILS_SELECTORS } from '../../store/currentGameSlice.ts';
+import * as React from 'react';
+import GameEndedDialog from '../game-ended-dialog';
 
-function SetScore() {
+export interface SetScoreProps {
+	gameEnded: boolean;
+}
+
+function SetScore(props: SetScoreProps) {
+	const { gameEnded } = props;
+	const navigate = useNavigate();
+	const winner = useSelector(CURRENT_TURN_DETAILS_SELECTORS.winnerOfGame);
+	const [openGameEndedDialog, setOpenGameEndedDialog] = React.useState(gameEnded);
+
 	return (
-		<div className='mt-auto flex items-center justify-center pt-10'>
-			<Typography variant='h2' className='h-[3em] w-full text-[2em] text-white'>
-				<Link to='/submit-score'>
-					<button className='h-full w-full'>Set Score</button>
-				</Link>
-			</Typography>
-		</div>
+		<>
+			<div className='mt-auto flex items-center justify-center pt-10'>
+				<Typography variant='h2' className='h-[3em] w-full text-[2em] text-white'>
+					<button
+						className='h-full w-full'
+						onClick={() => {
+							if (winner == undefined) {
+								navigate({ to: '/submit-score' });
+								return;
+							}
+							setOpenGameEndedDialog(true);
+						}}
+					>
+						Set Score
+					</button>
+				</Typography>
+			</div>
+			{(openGameEndedDialog && winner != undefined) &&
+				<GameEndedDialog
+					keepMounted={false}
+					open={openGameEndedDialog}
+					team={winner}
+					onClose={() => {
+						setOpenGameEndedDialog(false);
+					}}
+				/>
+			}
+		</>
 	);
 }
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,15 +2,18 @@ import TeamsNamesAndScores from '../../components/teams-names-and-scores';
 import SetScore from '../../components/set-score-button';
 import NavBar from '../../components/navbar';
 import TeamsMembersAndTichuControls from '../../components/teams_members_and_tichu_controls';
+import { useSelector } from 'react-redux';
+import { CURRENT_TURN_DETAILS_SELECTORS } from '../../store/currentGameSlice.ts';
 
 function Index() {
+	const winnerOfGame = useSelector(CURRENT_TURN_DETAILS_SELECTORS.winnerOfGame);
 	return (
 		<div className='fixed flex h-full w-full flex-col'>
 			<NavBar />
 			<main className='flex h-full w-full flex-col overflow-hidden'>
 				<TeamsNamesAndScores />
 				<TeamsMembersAndTichuControls />
-				<SetScore />
+				<SetScore gameEnded={winnerOfGame != undefined} />
 			</main>
 		</div>
 	);
