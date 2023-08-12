f5254ca85d714e50616bd6c6685e6bb696a046ce
Rafael Triantafillidis <botazz133@gmail.com>
2023-08-12 16:35:22 +0300

    Add links to about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -9,7 +9,17 @@ function About() {
 				<div className='flex flex-col justify-center items-center h-full w-full'>
 					<h1>Hehe</h1>
 					<h2>Tichu counter app :)</h2>
-					<h2>Made by Raf</h2>
+					<h2>
+						Made by {' '}
+						<a className='text-blue-500' href='https://yr5.es'>
+							Raf
+						</a>
+					</h2>
+					<h2 className='text-blue-500'>
+						<a href='https://github.com/alator21/grand-tichu'>
+							Github
+						</a>
+					</h2>
 				</div>
 			</main>
 		</div>
